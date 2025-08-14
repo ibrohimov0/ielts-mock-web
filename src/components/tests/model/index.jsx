b838f820b5209494caa60dfd440439d08ea03a22
Abdulloh <ibrohimovabdulloh573@gmail.com>
2025-08-14 15:27:41 +0500

    Update README with project setup instructions; remove old README file and clean package metadata

--- src/components/tests/model/index.jsx
+++ src/components/tests/model/index.jsx
@@ -1,8 +1,13 @@
+import { GetTests } from "../../../shared/api/getTests";
 import "../ui/style.css"
 import { useQuery } from "@tanstack/react-query"
 
 export default function TestFetch() {
-    const { data, error, isLoading } = useQuery(['tests'], GetTests);
+
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['tests'],
+        queryFn: GetTests,
+    });
 
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
